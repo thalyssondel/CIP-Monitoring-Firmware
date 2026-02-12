c8e14023e48a6ec1afb1550ee2a0c79f81075d0d
Thalysson Delano <contato.thalyssondel@gmail.com>
2026-02-12 15:22:34 -0300

    add: example for secrets.cmake

---
+++ secrets.example.cmake
@@ -0,0 +1,13 @@
+# --- Ponto de Conexão ---
+set(TARGET_SERVER_IP "TARGET_SERVER_IP")
+set(TARGET_PORT PORT)
+set(TARGET_PATH "TARGET_PATH")
+
+# --- Identidade de Rede do Dispositivo ---
+set(ETHERNET_MAC "ETHERNET_MAC")
+set(DEVICE_IP "DEVICE_IP")
+set(GATEWAY_IP "GATEWAY_IP")
+set(SUBNET_MASK "SUBNET_MASK")
+
+# --- Auth---
+set(BEARER_TOKEN "BEAREN_TOKEN")
